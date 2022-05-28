fcbf9a45933e62d3fa226be356d865d51cee1c58
MelihUslu <m.melihuslu@gmail.com>
2022-05-28 14:12:06 +0300

    initial commit

--- src/main/java/com/akis_yazilim/postgre/admin/product/SpringBootMicroserviceProductApplication.java
+++ src/main/java/com/akis_yazilim/postgre/admin/product/SpringBootMicroserviceProductApplication.java
@@ -3,9 +3,12 @@ package com.akis_yazilim.postgre.admin.product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
-public class SpringBootMicroserviceProductApplication {
+@PropertySource("classpath:application-${spring.profiles.active:default}.properties")
+public class SpringBootMicroserviceProductApplication
+{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMicroserviceProductApplication.class, args);
